98fbd56e9780449877eca43fcdbd1f5e27cc0a21
Levi Barroga <barroga.l.bscs@gmail.com>
2025-07-22 14:27:16 +0800

    Layout the structure of Python GUI for Eminem quotes.

---
+++ main.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from tkinter import ttk
+import requests
+
+
+url = 'https://eminem-quotes-api.onrender.com/'
+
+class LyricsApp:
+
+    def __init__(self, root):
+        root.title('Random Lyrics from Eminem')
+
+        self.canvas = Canvas()
+        mainframe = ttk.Frame(root)
+        mainframe.grid(column=0, row=0)
+        root.columnconfigure(0, weight =1)
+        root.rowconfigure(0, weight=1)
+
+        self.canvas = Canvas(mainframe, width=400, height=200)
+        self.canvas.grid(column=0, row=0, columnspan=4, sticky="nsew")
+        self.quote_s = self.canvas.create_text(100,100,text='placeholder', width=50,font='Arial')
+        ttk.Button(mainframe, text='Generate', command=self.the_quote).grid(column=3,row=3,sticky=W)
+
+    def the_quote(self, *args):
+        try:
+            clicked = requests.get(url)
+            if clicked.status_code == 200:
+                data = clicked.json()
+                quotes = data['quotes']
+                self.canvas.itemconfig(self.quote_s, text= quotes)
+        except ValueError:
+            pass
+
+
+
+root = Tk()
+LyricsApp(root)
+root.mainloop()
