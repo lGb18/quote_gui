02fc7ba6aac49efa1b16746628faa5593d3011b9
Levi Barroga <barroga.l.bscs@gmail.com>
2025-07-23 19:38:50 +0800

    Text label and button aligment.

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ class LyricsApp:
 
         self.canvas = Canvas()
         mainframe = ttk.Frame(root)
-        mainframe.grid(column=0, row=0)
+        mainframe.grid(column=10, row=10)
         root.columnconfigure(0, weight =1)
         root.rowconfigure(0, weight=1)
 
-        self.canvas = Canvas(mainframe, width=400, height=200)
+        self.canvas = Canvas(mainframe, width=800, height=600, bg='red')
         self.canvas.grid(column=0, row=0, columnspan=4, sticky="nsew")
-        self.quote_s = self.canvas.create_text(100,100,text='placeholder', width=50,font='Arial')
-        ttk.Button(mainframe, text='Generate', command=self.the_quote).grid(column=3,row=3,sticky=W)
+        self.quote_s = self.canvas.create_text(400,300,text='placeholder', width=600,font=('Arial', 32, 'bold'))
+        ttk.Button(mainframe, text='Generate', command=self.the_quote).grid(column=2,row=1, sticky=W)
 
     def the_quote(self, *args):
         try:
