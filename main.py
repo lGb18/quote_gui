0574cbbd317406cb04274d1733a34cddfd0535b5
Levi Barroga <barroga.l.bscs@gmail.com>
2025-07-25 10:57:16 +0800

    Align bubble image with quote and button.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 import requests
 
 
-url = 'https://api.kanye.rest/'
+url = 'https://eminem-quotes-api.onrender.com/'
 
 class LyricsApp:
 
@@ -16,20 +16,22 @@ class LyricsApp:
         mainframe = ttk.Frame(root)
         mainframe.grid(column=0, row=0)
         root.columnconfigure(0, weight =1)
-        root.rowconfigure(0, weight=1)
 
         bubble_image = Image.open("speech-bubble-png-13.png").convert("RGBA")
         bubble_image = bubble_image.resize((700, 600))
         speech_bubble = ImageTk.PhotoImage(bubble_image)
 
         self.canvas = Canvas(mainframe, width=800, height=600)
-        self.canvas.create_image(400, 350, image=speech_bubble)
+        self.canvas.create_image(400, 300, image=speech_bubble)
         self.canvas.grid(column=0, row=0, columnspan=4, sticky="nsew")
-        self.quote_s = self.canvas.create_text(400,250,text='placeholder', width=400,font=('Arial', 28, 'bold'))
+        self.quote_s = self.canvas.create_text(400,250,text='placeholder', width=500,font=('Arial', 28, 'bold'))
 
-        mini_image = ImageTk.PhotoImage(Image.open("SlimShady.png").convert("RGBA"))
+        pil_image = Image.open("SlimShady.png").convert("RGBA")
+        resized = pil_image.resize((155,155))
+        mini_image = ImageTk.PhotoImage(resized)
+        # mini_image = ImageTk.PhotoImage(Image.open("SlimShady.png").convert("RGBA"))
         em_button = ttk.Button(root, image=mini_image, command=self.the_quote, )
-        em_button.grid(column=0, row=1, sticky=W)
+        em_button.grid(column=0, row=1, sticky=N)
 
         em_button.image = mini_image
         self.canvas.image = speech_bubble
@@ -38,7 +40,7 @@ class LyricsApp:
             clicked = requests.get(url)
             if clicked.status_code == 200:
                 data = clicked.json()
-                quotes = data['quote']
+                quotes = data['quotes']
                 self.canvas.itemconfig(self.quote_s, text= quotes)
         except ValueError:
             pass
